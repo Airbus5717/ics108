6e8d68e2ec7b5e0461dce04982d54b9c7fab3580
Airbus5717 <Abdussamadf@hotmail.com>
2021-06-14 16:07:10 +0300

    added lab2

---
+++ labs/lab2/Task1.java
@@ -0,0 +1,23 @@
+package com.airbus5717.lab2;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first 12 digits of an ISBN-13 as a string: ");
+        String number = input.next();
+        if (number.length() != 12) {
+            System.out.println(number + " is an invalid input");
+            System.exit(1);
+        }
+        int sum = 0;
+        for (int i = 0; i < number.length(); i++) {
+            if (i % 2 != 0) sum += (number.charAt(i) - '0') * 3;
+            else sum += number.charAt(i) - '0';
+        }
+        sum = 10 - sum % 10;
+        System.out.println("The ISBN-13 number is " + number + (sum != 10 ? sum : 0));
+    }
+}
---
+++ labs/lab2/Task2.java
@@ -0,0 +1,25 @@
+package com.airbus5717.lab2;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter milliseconds: ");
+        long millis = input.nextLong();
+        System.out.println(convertMillis(millis));
+    }
+
+    public static String convertMillis(long millis) {
+        long sec = millis / 1000;
+        long seconds = sec % 60;
+        long minutes = sec / 60;
+        if (minutes >= 60) {
+            long hours = minutes / 60;
+            minutes %= 60;
+            return millis + " milliseconds = " + hours + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
+        }
+        return millis + " milliseconds = "+ "00" + ":" + minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
+    }
+
+}
